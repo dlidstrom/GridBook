de07ec8212a912c1f76c117bceeeee4f03f33afd
Daniel Lidstrom <dlidstrom@gmail.com>
2011-01-30 22:31:55 +0100

    Added dumper class.
    This class will be used to create dump files that will
    be imported to MySql.

---
+++ GridBook.Test/DumperTest.cs
@@ -0,0 +1,69 @@
+﻿namespace GridBook.Test
+{
+	using System;
+	using System.IO;
+	using GridBook.Service;
+	using NUnit.Framework;
+	using System.Text;
+
+	[TestFixture]
+	public class DumperTest
+	{
+		private Dumper dumper;
+
+		[SetUp]
+		public void Setup()
+		{
+			// Arrange
+			dumper = new Dumper("Data/JA_s12.book");
+		}
+
+		[Test]
+		public void DumperCanGenerateHeader()
+		{
+			// Act
+			using (var stream = new MemoryStream())
+			{
+				dumper.WriteHeading(stream);
+				// Assert
+				var result = Encoding.Default.GetString(stream.GetBuffer());
+				var heading = "/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;\r\n"
+					+ "/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;\r\n"
+					+ "/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;\r\n"
+					+ "/*!40101 SET NAMES utf8 */;\r\n"
+					+ "/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;\r\n"
+					+ "/*!40103 SET TIME_ZONE='+00:00' */;\r\n"
+					+ "/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;\r\n"
+					+ "/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;\r\n"
+					+ "/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;\r\n"
+					+ "/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;\r\n";
+				Assert.AreEqual(heading, result);
+			}
+		}
+
+		[Test]
+		public void DumperCanGenerateBoardTable()
+		{
+			// Act
+			using (var stream = new MemoryStream())
+			{
+				dumper.WriteBoardTable(stream);
+				// Assert
+				var result = Encoding.Default.GetString(stream.GetBuffer());
+				var table = "DROP TABLE IF EXISTS `board`;\r\n"
+					+ "/*!40101 SET @saved_cs_client     = @@character_set_client */;\r\n"
+					+ "/*!40101 SET character_set_client = utf8 */;\r\n"
+					+ "CREATE TABLE `board` (\r\n"
+					+ "  `Id` varchar(40) NOT NULL,\r\n"
+					+ "  `Empty` bigint(20) DEFAULT NULL,\r\n"
+					+ "  `Mover` bigint(20) DEFAULT NULL,\r\n"
+					+ "  `Ply` int(11) NOT NULL,\r\n"
+					+ "  PRIMARY KEY (`Id`),\r\n"
+					+ "  UNIQUE KEY `Empty` (`Empty`,`Mover`)\r\n"
+					+ ") ENGINE=InnoDB DEFAULT CHARSET=latin1;\r\n"
+					+ "/*!40101 SET character_set_client = @saved_cs_client */;\r\n";
+				Assert.AreEqual(table, result);
+			}
+		}
+	}
+}
---
+++ GridBook/Service/Dumper.cs
@@ -0,0 +1,55 @@
+﻿namespace GridBook.Service
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.IO;
+
+	public class Dumper
+	{
+		private string p;
+
+		public Dumper(string p)
+		{
+			// TODO: Complete member initialization
+			this.p = p;
+		}
+
+		public void WriteHeading(Stream stream)
+		{
+			using (var writer = new StreamWriter(stream))
+			{
+				writer.WriteLine("/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;");
+				writer.WriteLine("/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;");
+				writer.WriteLine("/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;");
+				writer.WriteLine("/*!40101 SET NAMES utf8 */;");
+				writer.WriteLine("/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;");
+				writer.WriteLine("/*!40103 SET TIME_ZONE='+00:00' */;");
+				writer.WriteLine("/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;");
+				writer.WriteLine("/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;");
+				writer.WriteLine("/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;");
+				writer.WriteLine("/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;");
+			}
+		}
+
+		public void WriteBoardTable(Stream stream)
+		{
+			using (var writer = new StreamWriter(stream))
+			{
+				writer.WriteLine("DROP TABLE IF EXISTS `board`;");
+				writer.WriteLine("/*!40101 SET @saved_cs_client     = @@character_set_client */;");
+				writer.WriteLine("/*!40101 SET character_set_client = utf8 */;");
+				writer.WriteLine("CREATE TABLE `board` (");
+				writer.WriteLine("  `Id` varchar(40) NOT NULL,");
+				writer.WriteLine("  `Empty` bigint(20) DEFAULT NULL,");
+				writer.WriteLine("  `Mover` bigint(20) DEFAULT NULL,");
+				writer.WriteLine("  `Ply` int(11) NOT NULL,");
+				writer.WriteLine("  PRIMARY KEY (`Id`),");
+				writer.WriteLine("  UNIQUE KEY `Empty` (`Empty`,`Mover`)");
+				writer.WriteLine(") ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+				writer.WriteLine("/*!40101 SET character_set_client = @saved_cs_client */;");
+			}
+		}
+	}
+}
