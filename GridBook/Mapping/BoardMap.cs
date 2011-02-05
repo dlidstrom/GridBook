cba17cfcb0779e89739973fdf8a385be34447f7d
Daniel Lidstrom <dlidstrom@gmail.com>
2011-02-05 15:37:05 +0100

    Custom Id generator for Board class.
    This Id generator uses the empty and mover
    values to create a Guid. The purpose is to be
    able to create Ids in a predictable way.

--- GridBook/Mapping/BoardMap.cs
+++ GridBook/Mapping/BoardMap.cs
@@ -1,13 +1,13 @@
 ﻿namespace GridBook.Domain.Mapping
 {
 	using FluentNHibernate.Mapping;
-	using FluentNHibernate.MappingModel.Collections;
+	using GridBook.Mapping;
 
 	public class BoardMap : ClassMap<Board>
 	{
 		public BoardMap()
 		{
-			Id(x => x.Id);
+			Id(x => x.Id).GeneratedBy.Custom<CustomGuidGenerator>();
 			NaturalId().Property(x => x.Empty).Property(x => x.Mover);
 			Map(x => x.Ply).Not.Nullable();
 			HasManyToMany(x => x.Successors)
@@ -21,7 +21,7 @@
 			    .Access.ReadOnlyPropertyThroughLowerCaseField()
 			    .ParentKeyColumn("ChildId")
 			    .ChildKeyColumn("ParentId")
-				.Cascade.All() // test
+				.Cascade.All()
 			    .AsSet()
 			    .Table("Parents");
 		}
---
+++ GridBook/Mapping/CustomGuidGenerator.cs
@@ -0,0 +1,35 @@
+﻿namespace GridBook.Mapping
+{
+	using System;
+	using System.Collections.Generic;
+	using GridBook.Domain;
+	using NHibernate.Engine;
+	using NHibernate.Id;
+
+	/// <summary>
+	/// A custom Id generator. This generator uses the Empty and Mover
+	/// properties of Board to generate a Guid.
+	/// </summary>
+	public class CustomGuidGenerator : IIdentifierGenerator
+	{
+		/// <summary>
+		/// Generate a new identifier.
+		/// </summary>
+		/// <param name="session">The ISessionImplementor this id is being generated in.</param>
+		/// <param name="obj">The entity for which the id is being generated.</param>
+		/// <returns>The new identifier.</returns>
+		public object Generate(ISessionImplementor session, object obj)
+		{
+			// can only be used with Board
+			var board = obj as Board;
+			if (board == null)
+			{
+				throw new InvalidOperationException("CustomGuidGenerator can only be used with Board");
+			}
+
+			var bytes = new List<byte>(BitConverter.GetBytes(board.Empty));
+			bytes.AddRange(BitConverter.GetBytes(board.Mover));
+			return new Guid(bytes.ToArray());
+		}
+	}
+}
