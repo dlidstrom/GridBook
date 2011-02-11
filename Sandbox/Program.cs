87107dd7b77351ae29ccfd57da0e13e10d5346b6
Daniel Lidstrom <dlidstrom@gmail.com>
2011-02-11 23:54:17 +0100

    Testing.

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -6,29 +6,13 @@
 	using GridBook.Domain;
 	using System.Diagnostics;
 
-	class BoardComparer : Comparer<Board>
-	{
-		public override int Compare(Board x, Board y)
-		{
-			if (x < y)
-			{
-				return -1;
-			}
-			else if (x == y)
-			{
-				return 0;
-			}
-			else
-				return 1;
-		}
-	}
-
 	public class Program
 	{
 		static void Main(string[] args)
 		{
-			var board = Board.Start;
-			var successors = board.CalculateSuccessors();
+			Console.WriteLine("Position:");
+			Console.WriteLine(Board.FromGuid(new Guid("01000001-c303-ffff-fe1f-3e1c80300000")).MinimalReflection().ToGuid());
+			Console.WriteLine(Board.FromGuid(new Guid("80000080-c3c0-ffff-7ff8-7c38010c0000")).MinimalReflection().ToGuid());
 		}
 	}
 }
