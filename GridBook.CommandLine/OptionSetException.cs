f0a629905d103d03bd676639906a1d8baf8c6d63
Daniel Lidstrom <dlidstrom@gmail.com>
2011-02-27 12:09:21 +0100

    Added missing constructors.

--- GridBook.CommandLine/OptionSetException.cs
+++ GridBook.CommandLine/OptionSetException.cs
@@ -2,9 +2,29 @@
 {
 	using System;
 	using NDesk.Options;
+using System.Runtime.Serialization;
 
 	public class OptionSetException : Exception
 	{
+		protected OptionSetException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+
+		public OptionSetException()
+		{
+		}
+
+		public OptionSetException(string message)
+			: base(message)
+		{
+		}
+
+		public OptionSetException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+
 		public OptionSetException(OptionSet set)
 		{
 			this.OptionSet = set;
